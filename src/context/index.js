e1b8113eb3106ce9a85d4dedc4ee2841161f7fdb
fmarcos38 <fmarcos_23@hotmail.com>
2024-10-31 20:16:12 -0300

    login

--- src/context/index.js
+++ src/context/index.js
@@ -1,18 +1,61 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { userData } from "../localStorage";
 
-//creo contexto
+//creo el contexto 
 export const InmobiliariaContext = createContext();
 
-//creo el componente Provider para el contexto
+//creo provider, aquí van estados y funciones actualizadoras globales
 export const InmobiliariaProvider = ({children}) => {
-    
-    return(
-        <InmobiliariaContext.Provider
-            value={{
 
+    //estado data usuario logeado, por eso null es un objeto
+    const [userLog, setUserLog] = useState(null); console.log("UserDataLog:", userLog)
+    //estado para login
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    //estado nombre admin logeado
+    const [nombreUser, setNombreUser] = useState('');
+    //estado para menú hamburguesa
+    const [ isOpenModalVideo, setisOpenModalVideo ] = useState(false);
+
+    const login = () => {
+        setIsAuthenticated(true);
+    };
+    const logout = () => {
+        setIsAuthenticated(false);
+    };
+    const handleIsOpen = () => {
+        setisOpenModalVideo(true);
+    }
+    const handleIsClose = () => {
+        setisOpenModalVideo(false);
+    }
+
+    //efecto para el login
+    useEffect(()=>{
+        const userLog = userData();
+        if(userLog){
+            if(userData.esAdmin){
+                setUserLog(userLog);
+                setIsAuthenticated(true);
+                setNombreUser(userLog.user?.nombre);
+            }
+        }
+    },[]);
+
+
+    return (
+        <InmobiliariaContext.Provider 
+            value={{
+                userLog,
+                isAuthenticated,
+                nombreUser,
+                login,
+                logout,
+                isOpenModalVideo,
+                handleIsOpen,
+                handleIsClose,
             }}
         >
             {children}
         </InmobiliariaContext.Provider>
-    )
+    );
 };
